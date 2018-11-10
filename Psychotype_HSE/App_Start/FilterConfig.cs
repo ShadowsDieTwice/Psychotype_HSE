53453b515156e0f0767708cc6a6cc1397b56c657
Maxim <myurachinskiy@edu.hse.ru>
2018-11-10 20:59:51 +0300

    Добавьте файлы проекта.

---
+++ Psychotype_HSE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Psychotype_HSE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
