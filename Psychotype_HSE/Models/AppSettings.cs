60e889c42509e2d40b7c68555ed901f2ba15afa6
Maxim <myurachinskiy@edu.hse.ru>
2018-11-10 21:12:37 +0300

    - Implementation of vkApi class - Creation appSettings class

---
+++ Psychotype_HSE/Models/AppSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Psychotype.Models
+{
+    /// <summary>
+    /// All app settings which is required 
+    /// </summary>
+    public static class AppSettings
+    {
+        /// <summary>
+        /// Id of registered VK application (VkApi works only beyond such applications)
+        /// </summary>
+        public static ulong ApplicationId { get; set; }
+        /// <summary>
+        /// Login of user application owner 
+        /// </summary>
+        public static string Login { get; set; }
+        /// <summary>
+        /// Password of user application owner
+        /// </summary>
+        public static string Password { get; set; }
+
+        static AppSettings()
+        {
+            // Here we need to initialize all properties with fake account information
+        }
+
+    }
+}
