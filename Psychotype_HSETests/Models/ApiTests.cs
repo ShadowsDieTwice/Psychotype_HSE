1df72c3565343543926dc848e6ff8fb5b1f7e9d6
Maxim <myurachinskiy@edu.hse.ru>
2018-11-12 17:54:29 +0300

    Making initial tests directory

---
+++ Psychotype_HSETests/Models/ApiTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Psychotype_HSE_Tests.Models
+{
+    [TestClass()]
+    public class ApiTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
