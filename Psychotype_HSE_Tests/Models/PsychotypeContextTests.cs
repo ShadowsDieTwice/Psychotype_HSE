2896f4dcb124403713a93bb8b350b2dafe326d1f
Maxim <myurachinskiy@edu.hse.ru>
2019-01-17 23:56:08 +0300

    Words in database - Redundant words from search - Suicide likely words

---
+++ Psychotype_HSE_Tests/Models/PsychotypeContextTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Psychotype_HSE.Models;
+
+namespace Psychotype_HSE_Tests.Models
+{
+	[TestClass()]
+	public class PsychotypeContextTests
+	{
+		private PsychotypeContext database = new PsychotypeContext();
+
+		[TestMethod()]
+		public void DatabaseAddTest()
+		{
+			database.RedundantWords.Add(new RedundantWord { Word = "Check" });
+			database.SuicideWords.Add(new SuicideWord {Word = "This"});
+			database.SaveChanges();
+
+			Assert.IsTrue(database.RedundantWords.Any(word => word.Word == "Check"));
+			Assert.IsTrue(database.SuicideWords.Any(word => word.Word == "This"));
+		}
+	}
+}
