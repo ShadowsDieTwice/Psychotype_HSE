e5381100d774b0f64a9c647e491f969bbbba369b
dsfedorovichev <dsfedorovichev@edu.hse.ru>
2019-01-18 18:43:19 +0300

    Index page only

--- Psychotype_HSE/Controllers/HomeController.cs
+++ Psychotype_HSE/Controllers/HomeController.cs
@@ -13,25 +13,5 @@ namespace Psychotype_HSE.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult DefaultTemplate()
-        {
-
-            return View();
-        }
-
     }
 }
